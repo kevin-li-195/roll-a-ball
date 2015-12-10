d05e46a410490bab35489ef0c7cf00f371f9e9ad
Kevin Li <kevin.li195@hotmail.com>
2015-12-09 22:40:37 -0500

    Finish project, basic game is working.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	private Vector3 offset;
+	public GameObject player;
+	void Start () {
+		offset = this.transform.position - player.transform.position;
+	}
+	
+	void LateUpdate () {
+		this.transform.position = player.transform.position + offset;	
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 10.0f;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count;
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 mvmt = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (mvmt * speed);
+	}
+
+	void SetCountText () {
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 9) {
+			winText.text = "You win!";
+		}
+	}
+
+	void OnTriggerEnter(Collider a) {
+		if (a.gameObject.CompareTag ("Pickup")) {
+			a.gameObject.SetActive (false);	
+			count += 1;
+			SetCountText();
+		}
+	}
+}
